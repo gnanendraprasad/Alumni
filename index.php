8d6f7593e07618ccd6d89fc8f3746e65dd6fd9c5
ARUN KUMAR M <marunkumar664@gmail.com>
2019-11-06 20:32:38 +0530

    Structured. User, Landing page and Admin

--- index.php
+++ index.php
@@ -1,97 +1,59 @@
-<?php
-
-if (isset($_GET['error'])) {
-	if ($_GET['error']=='emptyfields') {
-		echo '<script>alert("Fill all the details")</script>';
-	}
-	else if ($_GET['error']=='sqlerror') {
-		echo '<script>alert("Refresh and try again")</script>';
-	}
-	else if ($_GET['error']=='invalidPassword') {
-		echo '<script>alert("Incorrect password")</script>';
-	}
-	else if ($_GET['error']=='invalidemail') {
-		echo '<script>alert("Invalid Email")</script>';
-	}
-	else if ($_GET['error']=='invalidusername') {
-		echo '<script>alert("Invalid Username")</script>';
-	}
-	else if ($_GET['error']=='invalidmailuid') {
-		echo '<script>alert("Invalid email and username")</script>';
-	}
-	else if($_GET['error']=='useralreadyexist') {
-		echo '<script>alert("User Already Exist, wait for mail")</script>';
-	}
-	else if($_GET['error']=='useralreadysignedup') {
-		echo '<script>alert("User Already Exist, wait for acceptance.")</script>';
-	}
-}
-
-if (isset($_GET['signup'])) {
-	if ($_GET['signup']=='success') {
-		echo '<script>alert("SignUp Success. we will send u mail shortly")</script>';
-	}
-}
-?>
-
-<!DOCTYPE html>
 <html>
-<head>
-	<title>SignUp and Login</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-</head>
-<body>
+    <head>
+    <Title>Kssem Connect</Title>
 
-<div class="container" id="container">
-<div class="form-container sign-up-container">
+    <link rel="stylesheet" type="text/css" href="landing/css/nav.css">
+    <link rel="stylesheet" type="text/css" href="landing/css/slider.css">
+    </head>
+    <body>
+    <div class="container">    
 
-<form action="includes/signup.php" method="POST">
-	<h3>Please provide your details!!</h3>
-        <!--<div class="social-container">
-		<a href="#" class="social"><i class="fa fa-facebook"></i></a>
-		<a href="#" class="social"><i class="fa fa-google"></i></a>
-		<a href="#" class="social"><i class="fa fa-linkedin"></i></a>
-	</div>
-	<span>or use your email for registration</span>  -->
-	<input type="text" name="usn" placeholder="USN">
-	<input type="text" name="name" placeholder="Name">
-	<input type="email" name="email" placeholder="Email">
-	<button type="submit" name="submit-signup">Submit</button>
-</form>
-</div>
-<div class="form-container sign-in-container">
-	<form action="includes/signin.php" method="POST">
-		<h1>Sign In</h1>
-		<!-- <div class="social-container">
-		<a href="#" class="social"><i class="fa fa-facebook"></i></a>
-		<a href="#" class="social"><i class="fa fa-google"></i></a>
-		<a href="#" class="social"><i class="fa fa-linkedin"></i></a>
-	</div>
-	<span>or use your account</span> -->
-	<input type="text" name="email" placeholder="USN or Email">
-	<input type="password" name="password" placeholder="Password">
-	<a href="#">Forgot Your Password</a>
+        <!-- nav-menu -->
+        <div class="nav" id="nav">
+        <ul>
+            <li><a href="user/userlogin.php">Login/Register</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Events & News</a></li>
+            <li><a href="#">About Us</a></li>
+            <li class="active" id="#" ><a href="#">Home</a></li>
+        </ul>
+        </div>
 
-	<button>Sign In</button>
-	</form>
-</div>
-<div class="overlay-container">
-	<div class="overlay">
-		<div class="overlay-panel overlay-left">
-			<h1>Welcome Back!</h1>
-			<p>To keep connected with us please login with your personal info</p>
-			<button class="ghost" id="signIn">Sign In</button>
-		</div>
-		<div class="overlay-panel overlay-right">
-			<h1>Hello, Friend!</h1>
-			<p>Enter your details and start journey with us</p>
-			<button class="ghost" id="signUp">Sign Up</button>
-		</div>
-	</div>
-</div>
-</div>
-<script type="text/javascript" src="js/signinc.js"></script>
-</body>
-</html>
+        <!-- Slider -->
+        <!-- Just a css slider, need enhancement for better one -->
+        <div id="slider">
+        <figure>
+        <img src="landing/img/img1.jpg" alt>
+        <img src="landing/img/img2.jpg" alt>
+        <img src="landing/img/img3.jpg" alt>
+        <img src="landing/img/img2.jpg" alt>
+        <img src="landing/img/img3.jpg" alt>
+        </figure>
+        </div>
+
+        <!-- News , Events & Notable Alumni -->
+        <div class="multiple">
+            <ul class="multislide">
+            <li><a class="news" href="#">NEWS</a></li>
+            <li><a class="events" href="#">EVENTS</a><li>
+            <li><a class="notable" href="#">NOTABLE ALUMNI</a><li>
+            </ul>
+        </div>
+                <!-- - // News , Events & Notable Alumni -->
+
+
+
+
+
+
+
+
+
+
+    </div>
+        <script type="text/js" href="landing/js/activeclass.js"></script>
+        <script type="text/js" href="landing/js/slider.js"></script> 
+        <script type="text/js" href="#"></script>        
+    </body>
+</html>    
