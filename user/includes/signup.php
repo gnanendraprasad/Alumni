4d55312fcd006cea5f2cb23c425f8c2b20be42ad
GNANENDRA PRASAD <gnanendra097@gmail.com>
2019-11-10 20:35:51 +0530

    signup

--- user/includes/signup.php
+++ user/includes/signup.php
@@ -7,29 +7,28 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $salt = "alumni";
 $password = $_POST["usn"];
-/*$password_encrypted = sha1($password.$salt);*/
 
 if (empty($name) || empty($email) || empty($password)) {
-	header("Location: ../index.php?error=emptyfields&uid=.$name.&mail=.$email");
+	header("Location: ../userlogin.php?error=emptyfields&uid=.$name.&mail=.$email");
 	exit();
 }
 elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-	header("Location: ../index.php?error=invalidemail&uid=.$name");
+	header("Location: ../userlogin.php?error=invalidemail&uid=.$name");
 	exit();
 }
 elseif (!preg_match("/^[a-zA-Z]*$/", $name)) {
-	header("Location: ../index.php?error=invalidusername&mail=.$email");
+	header("Location: ../userlogin.php?error=invalidusername&mail=.$email");
 	exit();
 }
 elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z]*$/", $name)) {
-	header("Location: ../index.php?error=invalidemailuid");
+	header("Location: ../userlogin.php?error=invalidemailuid");
 	exit();
 }
 else{
 $sql="select usn from unreg where usn=?";
 $stmt=mysqli_stmt_init($conn);
 if(!mysqli_stmt_prepare($stmt,$sql)){
-	header("Location: ../index.php?error=sqlerror");
+	header("Location: ../userlogin.php?error=sqlerror");
 	exit();
 }
 else{
@@ -38,14 +37,14 @@ else{
 	mysqli_stmt_store_result($stmt);
 	$resultcheck=mysqli_stmt_num_rows($stmt);
 	if ($resultcheck>0) {
-		header("Location: ../index.php?error=useralreadyexist&mail=.$email");
+		header("Location: ../userlogin.php?error=useralreadyexist&mail=.$email");
 		exit();
 	}
 	else{
 		$sql="select user_usn from users where user_usn=?";
 		$stmt=mysqli_stmt_init($conn);
 		if(!mysqli_stmt_prepare($stmt,$sql)){
-			header("Location: ../index.php?error=sqlerror");
+			header("Location: ../userlogin.php?error=sqlerror");
 			exit();
 		}
 		else{
@@ -54,21 +53,21 @@ else{
 			mysqli_stmt_store_result($stmt);
 			$resultcheck=mysqli_stmt_num_rows($stmt);
 			if ($resultcheck>0) {
-				header("Location: ../index.php?error=useralreadysignedup&mail=.$email");
+				header("Location: ../userlogin.php?error=useralreadysignedup&mail=.$email");
 				exit();
 			}
 			else{
 				$sql="insert into unreg(name,email,usn) values(?,?,?)";
 				$stmt=mysqli_stmt_init($conn);
 				if(!mysqli_stmt_prepare($stmt,$sql)){
-					header("Location: ../index.php?error=sqlerror");
+					header("Location: ../userlogin.php?error=sqlerror");
 					exit();
 				}
 				else{
-					//$hashedpassword=password_hash($password, PASSWORD_DEFAULT); //used sha1 instead of hash
+
 					mysqli_stmt_bind_param($stmt,"sss",$name,$email,$password);
 					mysqli_stmt_execute($stmt);
-					header("Location: ../index.php?signup=success");
+					header("Location: ../userlogin.php?signup=success");
 					exit();
 			}
 			}
@@ -81,6 +80,6 @@ mysqli_stmt_close($stmt);
 mysqli_close($conn);
 }
 else{
-  header("Location: ../index.php");
+  header("Location: ../userlogin.php");
   exit();
 }
