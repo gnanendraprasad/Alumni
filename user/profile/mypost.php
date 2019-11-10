1b36454edc5778e54d7fd480d36776194d67dca2
GNANENDRA PRASAD <gnanendra097@gmail.com>
2019-11-10 17:39:26 +0530

    Display Post

--- user/profile/mypost.php
+++ user/profile/mypost.php
@@ -1,6 +1,7 @@
 <?php
-session_start();
-    include "../includes/headerpro.php"
+    session_start();
+    include "../includes/headerpro.php";
+    require "../includes/config.php";
 ?>
 <html>
   <head>
@@ -10,6 +11,26 @@ session_start();
     <div class="newpostbutton">
       <li id="cpost"><a href="newpost.php"><button>Create New Post</button></a>
     </div>
+    <div class="all-posts">
+      <?php
+      $sql="Select * from post";
+      if ($result = $conn->query($sql)) {
+
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        $field1name = $row["ptype"];
+        $field2name = $row["description"];
+
+        echo '<b>'.$field1name.$field2name.'</b><br />';
+    }
+
+    /* free result set */
+    $result->free();
+}
+?>
+    </div>
+
+
     <script type="text/javascript" src="../js/createpost.js"></script> <!-- Need to create alert, check the file by path and add function to alert -->
   </body>
 </html>
