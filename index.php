071d486ed84f1cbdbba5f936a2489f69e669f3af
ARUN KUMAR M <marunkumar664@gmail.com>
2019-11-15 23:58:36 +0530

    Landing page complete, starting with onetime.php

--- index.php
+++ index.php
@@ -1,59 +1,256 @@
-<html>
-    <head>
-    <Title>Kssem Connect</Title>
+<!DOCTYPE html>
+<html lang="en">
 
-    <link rel="stylesheet" type="text/css" href="landing/css/nav.css">
-    <link rel="stylesheet" type="text/css" href="landing/css/slider.css">
-    </head>
-    <body>
-    <div class="container">    
+<head>
 
-        <!-- nav-menu -->
-        <div class="nav" id="nav">
-        <ul>
-            <li><a href="user/userlogin.php">Login/Register</a></li>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">Gallery</a></li>
-            <li><a href="#">Events & News</a></li>
-            <li><a href="#">About Us</a></li>
-            <li class="active" id="#" ><a href="#">Home</a></li>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Welcome Alumni</title>
+
+  <!-- Font Awesome Icons -->
+  <link href="bootstraplanding/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="bootstraplanding/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="bootstraplanding/css/creative.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Student Connect</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">Get Started</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
         </ul>
-        </div>
-
-        <!-- Slider -->
-        <!-- Just a css slider, need enhancement for better one -->
-        <div id="slider">
-        <figure>
-        <img src="landing/img/img1.jpg" alt>
-        <img src="landing/img/img2.jpg" alt>
-        <img src="landing/img/img3.jpg" alt>
-        <img src="landing/img/img2.jpg" alt>
-        <img src="landing/img/img3.jpg" alt>
-        </figure>
-        </div>
-
-        <!-- News , Events & Notable Alumni -->
-        <div class="multiple">
-            <ul class="multislide">
-            <li><a class="news" href="#">NEWS</a></li>
-            <li><a class="events" href="#">EVENTS</a><li>
-            <li><a class="notable" href="#">NOTABLE ALUMNI</a><li>
-            </ul>
-        </div>
-                <!-- - // News , Events & Notable Alumni -->
-
-
-
-
-
-
-
-
-
-
+      </div>
     </div>
-        <script type="text/js" href="landing/js/activeclass.js"></script>
-        <script type="text/js" href="landing/js/slider.js"></script> 
-        <script type="text/js" href="#"></script>        
-    </body>
-</html>    
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">Welcome</h1>
+          <!-- <hr class="divider my-4"> -->
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5">You are the one soul that's been enlightened!!</p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- About Section -->
+  <section class="page-section bg-primary" id="about">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="text-white mt-0">Alumni Association!</h2>
+          <hr class="divider light my-4">
+          <p class="text-white-50 mb-4">
+            Greetings from Your Institution !!</p>
+            <p class="text-white-50 mb-4">College Management</p>
+          <a class="btn btn-light btn-xl js-scroll-trigger" href="user/userlogin.php">Stay Connected!</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+    <div class="container">
+      <h2 class="text-center mt-0">Why Alumni Association?</h2>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-user-friends text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Friendship</h3>
+            <p class="text-muted mb-0">The True bond, You'll stay connected with your friends no matter how far you are!!</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Up to Date</h3>
+            <p class="text-muted mb-0">You'll be updated with any events/news related to alumni.</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-gem text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Opportunities</h3>
+            <p class="text-muted mb-0">As a part of the association you will be able to find many opportunities within!</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-globe text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Spread Knowledge</h3>
+            <p class="text-muted mb-0">Not only check the events/news, you can conduct/organize events which benefits everyone!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Portfolio Section -->
+  <section id="portfolio">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="bootstraplanding/img/portfolio/fullsize/1.jpg">
+            <img class="img-fluid" src="bootstraplanding/img/portfolio/thumbnails/1.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Alumni
+              </div>
+              <div class="project-name">
+                Batch - 2015
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="bootstraplanding/img/portfolio/fullsize/2.jpg">
+            <img class="img-fluid" src="bootstraplanding/img/portfolio/thumbnails/2.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Alumni
+              </div>
+              <div class="project-name">
+                Batch - 2016
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="bootstraplanding/img/portfolio/fullsize/3.jpg">
+            <img class="img-fluid" src="bootstraplanding/img/portfolio/thumbnails/3.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Alumni
+              </div>
+              <div class="project-name">
+                Batch - 2017
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="bootstraplanding/img/portfolio/fullsize/4.jpg">
+            <img class="img-fluid" src="bootstraplanding/img/portfolio/thumbnails/4.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Alumni
+              </div>
+              <div class="project-name">
+                Batch - 2018
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="bootstraplanding/img/portfolio/fullsize/5.jpg">
+            <img class="img-fluid" src="bootstraplanding/img/portfolio/thumbnails/5.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Alumni
+              </div>
+              <div class="project-name">
+                Batch - 2019
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="bootstraplanding/img/portfolio/fullsize/6.jpg">
+            <img class="img-fluid" src="bootstraplanding/img/portfolio/thumbnails/6.jpg" alt="">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+                Continued..
+              </div>
+              <div class="project-name">
+                Coming soon!!
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Contact Section -->
+  <section class="page-section" id="contact">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0">Let's Get In Touch!</h2>
+          <hr class="divider my-4">
+          <p class="text-muted mb-5">Any help or assistance ?  Give us a call or send us an email and we will get back to you as soon as possible!</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+          <div>+91 (123) 456-7890</div>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <a class="d-block" href="mailto:institue@edu.in">institute@edu.in</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="small text-center text-muted">Copyright @2019 by Arun & Gnanendra</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="bootstraplanding/vendor/jquery/jquery.min.js"></script>
+  <script src="bootstraplanding/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="bootstraplanding/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="bootstraplanding/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="bootstraplanding/js/creative.min.js"></script>
+
+</body>
+
+</html>
