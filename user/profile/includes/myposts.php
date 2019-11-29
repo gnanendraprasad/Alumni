088b7f38e606e27de900c023188a61dbba4a650d
ARUN KUMAR M <marunkumar664@gmail.com>
2019-11-30 03:33:19 +0530

    css for news section is complete

--- user/profile/includes/myposts.php
+++ user/profile/includes/myposts.php
@@ -9,57 +9,137 @@
             <div class="header-posts">News</div>
                 <div class="post-contain">
                 <div class="force-overflow">
-                <div class="post-scrollbar" id="style-4">
+                <div class="post-scrollbar" id="style-8">
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            staying in the loop: you'll be informed of any career opportunities offered to IMES graduates
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            the newsletter will value lifelong learning: you will read articles from those who share common interests with you, and topics will include current research.
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            we'll be telling IMES on-going story, with all the exciting and interesting things our Department, staff, students and alumni are doing here and around the world
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            you'll receive invitations to receptions, seminars and other events not only from IMES but also from other organisations around the world, and these could be perfect opportunities to expand your professional connections and meet new people who share your interests
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            hearing from your achievements can bring about a whole lot of good for students who will be in the position you once occupied
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            staying in the loop: you'll be informed of any career opportunities offered to IMES graduates
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            the newsletter will value lifelong learning: you will read articles from those who share common interests with you, and topics will include current research.
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            we'll be telling IMES on-going story, with all the exciting and interesting things our Department, staff, students and alumni are doing here and around the world
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            you'll receive invitations to receptions, seminars and other events not only from IMES but also from other organisations around the world, and these could be perfect opportunities to expand your professional connections and meet new people who share your interests
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <div class="post-cover">
-                        <div class="post">Test 1</div>
-                        <hr class="hr">
-                    </div>
-                    <div class="post-cover">
-                        <div class="post">Test 1</div>
-                        <hr class="hr">
-                    </div>
-                    <div class="post-cover">
-                        <div class="post">Test 1</div>
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/new.gif" alt="new gif Image">
+                            hearing from your achievements can bring about a whole lot of good for students who will be in the position you once occupied
+                            </div>
+                        </div>
                         <hr class="hr">
                     </div>
                     <p></p>
