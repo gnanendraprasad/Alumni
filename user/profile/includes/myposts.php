0d6092d60297c14a7e05cd8d5d9ff71206941c98
GNANENDRA PRASAD <gnanendra097@gmail.com>
2019-12-01 11:05:36 +0530

    home page posts in database

--- user/profile/includes/myposts.php
+++ user/profile/includes/myposts.php
@@ -14,7 +14,42 @@
                 <div class="post-contain">
                 <div class="force-overflow">
                 <div class="post-scrollbar" id="style-8">
-                    <div class="post-cover">
+									<?php
+	                $sql="Select * from post";
+	                if ($result = $conn->query($sql)) {
+	                /* fetch associative array */
+	                while ($row = $result->fetch_assoc()) {
+	                        $fieldid = $row["pid"];
+	                        $field1name = $row["ptype"];
+	                        $field2name = $row["description"];
+
+	                        /*To set date and time for each post
+
+	                        date_default_timezone_set('Asia/Kolkata');
+	                        $timestamp = $_SERVER['REQUEST_TIME'];
+	                        $date = date('d/m/Y h:i:sa', time());
+	                        */
+	                        if($field1name=="NEWS"){
+	                echo '<div class="post-cover">
+                        <div class="post">
+                            <div class="datepost">
+                            <span>Nov<i>29</i>2019</span>
+                            </div>
+                            <div class="innercontent">
+                            <img src="img/icons/new.gif" alt="new gif Image">'.$field2name.'
+                            </div>
+                        </div>
+                        <hr class="hr">
+                    </div>';
+
+	                    }
+	                }
+	                /* free result set */
+	            $result->free();
+	            }
+	            ?>
+
+                  <!--  <div class="post-cover">
                         <div class="post">
                             <div class="datepost">
                             <span>Nov<i>29</i>2019</span>
@@ -145,27 +180,62 @@
                             </div>
                         </div>
                         <hr class="hr">
-                    </div>
+                    </div>-->
                     <p></p>
                     </div>
                 </div>
                 </div>
             <div class="content-posts"></div>
         </div>
-        
+
 <!-- News post ends -->
 
 <!-- Upcoming events starts -->
 		<div class="ue">
             <div class="header-posts">Upcoming Events</div>
             <div class="ue-contain" id="style-1">
-                <div class="ue-posts-cover">
+							<?php
+							$sql="Select * from post";
+							if ($result = $conn->query($sql)) {
+							/* fetch associative array */
+							while ($row = $result->fetch_assoc()) {
+											$fieldid = $row["pid"];
+											$field1name = $row["ptype"];
+											$field2name = $row["description"];
+
+											/*To set date and time for each post
+
+											date_default_timezone_set('Asia/Kolkata');
+											$timestamp = $_SERVER['REQUEST_TIME'];
+											$date = date('d/m/Y h:i:sa', time());
+											*/
+											if($field1name=="EVENT"){
+							echo '<div class="ue-posts-cover">
+									<div class="ue-posts">
+									<div class="ue-icon">
+									</div>
+									<div class="ue-info">
+											<div class="ue-desc">
+													<p>'.$field2name.'
+											</div>
+											</div>
+									</div>
+									<div class="ue-separator"></div>
+							</div>';
+
+									}
+							}
+							/* free result set */
+					$result->free();
+					}
+					?>
+                <!--<div class="ue-posts-cover">
                     <div class="ue-posts">
                     <div class="ue-icon">
                     </div>
                     <div class="ue-info">
                         <div class="ue-desc">
-                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values, 
+                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values,
                                 mission and culture of the company.<span>DateofEvent</span></p>
                         </div>
                         </div>
@@ -178,7 +248,7 @@
                     </div>
                     <div class="ue-info">
                         <div class="ue-desc">
-                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values, 
+                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values,
                                 mission and culture of the company.<span>DateofEvent</span></p>
                         </div>
                         </div>
@@ -191,7 +261,7 @@
                     </div>
                     <div class="ue-info">
                         <div class="ue-desc">
-                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values, 
+                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values,
                                 mission and culture of the company.<span>DateofEvent</span></p>
                         </div>
                         </div>
@@ -204,7 +274,7 @@
                     </div>
                     <div class="ue-info">
                         <div class="ue-desc">
-                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values, 
+                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values,
                                 mission and culture of the company.<span>DateofEvent</span></p>
                         </div>
                         </div>
@@ -217,7 +287,7 @@
                     </div>
                     <div class="ue-info">
                         <div class="ue-desc">
-                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values, 
+                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values,
                                 mission and culture of the company.<span>DateofEvent</span></p>
                         </div>
                         </div>
@@ -230,17 +300,17 @@
                     </div>
                     <div class="ue-info">
                         <div class="ue-desc">
-                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values, 
+                            <p>Job Description - Prior to posting a job, be thoughtful about the company profile and job description to ensure the posting speaks to the values,
                                 mission and culture of the company.<span>DateofEvent</span></p>
                         </div>
                         </div>
                     </div>
                     <div class="ue-separator"></div>
-                </div>
+                </div>-->
             </div>
             <div class="content-posts"></div>
         </div>
-        
+
 <!-- Upcoming events ends -->
 
 
@@ -248,7 +318,46 @@
 		<div class="j">
             <div class="header-posts">Jobs</div>
             <div class="job-contain" id="style-1">
-                <div class="job-posts-cover">
+							<?php
+							$sql="Select * from post";
+							if ($result = $conn->query($sql)) {
+							/* fetch associative array */
+							while ($row = $result->fetch_assoc()) {
+											$fieldid = $row["pid"];
+											$field1name = $row["ptype"];
+											$field2name = $row["description"];
+
+											/*To set date and time for each post
+
+											date_default_timezone_set('Asia/Kolkata');
+											$timestamp = $_SERVER['REQUEST_TIME'];
+											$date = date('d/m/Y h:i:sa', time());
+											*/
+											if($field1name=="JOB"){
+							echo '<div class="job-posts-cover">
+									<div class="job-posts">
+									<div class="job-icon">
+											<img src="img/icons/briefcase.png"/>
+									</div>
+									<div class="job-info">
+											<div class="job-desc">
+													<p>'.$field2name.'</div>
+											<div class="job-apply">
+													<input type="button" class="btn btn-secondary pull-left" id="learn-more"value="Learn More"/>
+													<input type="button" class="btn btn-primary pull-right" id="apply-now" value="Apply Now"/>
+											</div>
+											</div>
+									</div>
+									<div class="job-separator"></div>
+							</div>';
+
+									}
+							}
+							/* free result set */
+					$result->free();
+					}
+					?>
+                <!--<div class="job-posts-cover">
                     <div class="job-posts">
                     <div class="job-icon">
                         <img src="img/icons/briefcase.png"/>
@@ -366,7 +475,7 @@
                         </div>
                     </div>
                     <div class="job-separator"></div>
-                </div>
+                </div>-->
             </div>
 			<div class="content-posts"></div>
 		</div>
@@ -380,4 +489,4 @@
               $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
           }
       });
-</script> 
+</script>
